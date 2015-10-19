811962e62e6541f5377c528bccdfeffbefdd0f71
scvzxb <scvzxb@gmail.com>
2015-10-19 16:33:44 +0800

    first version of q2
    *using the basic method to solve the problem
    *the explanation can find in my hackpad

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,49 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+char smallest_character(char cmpInput, char *ch)
+{
+	int len;
+	len = strlen(ch);
+	printf("%d\n", len);
+	for (int i = 0; i < len; i++) {
+		if (cmpInput < ch[i])return ch[i];
+	}
+	return ch[0];
+}
+
 
 int main()
 {
-    return 0;
+	int numLetter = 0;
+	char rtnCh;
+	char cmpInput;
+	time_t start, end;
+	double timespend;
+
+
+	printf("how many alphabet?\n");
+	scanf("%d", &numLetter);
+	getchar();
+	char ch[20] = {0};
+	for (int i = 0; i < numLetter; i++ ) {
+		scanf("%c", &ch[i]);
+		getchar();
+	}
+	printf("enter a character\n");
+	scanf("%c", &cmpInput);
+	getchar();
+
+	start = clock();
+	rtnCh = smallest_character(cmpInput, ch);
+	end = clock();
+	timespend = (double) (end - start) / CLOCKS_PER_SEC;
+	printf("ANS : %c\ntime spend : %lf\n", cmpInput, timespend);
+	printf("the smallest characteris %c\n", rtnCh);
+
+	return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,45 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+char recursive(char cmpInput, char ch[], int count)
+{
+	if (cmpInput < ch[count - 1]) {
+		count--;
+		if (count < 0) {
+			return ch[0];
+		}
+		return recursive(cmpInput, ch, count);
+
+	} else if (count == strlen(ch) && cmpInput >= ch[count - 1]) {
+		return ch[0];
+	} else  {
+		return ch[count++];
+	}
+}
+
 
 int main()
 {
-    return 0;
+	double timespent;
+	clock_t start, end;
+	int numLetter = 0;
+	char cmpInput;
+	printf("how many alphabet?\n");
+	scanf(" %d", &numLetter);
+	char *ch = (char*)malloc(numLetter);
+	for (int i = 0; i < numLetter; i++ ) {
+		scanf(" %c", &ch[i]);
+	}
+	scanf(" %c", &cmpInput);
+	start = clock();
+	cmpInput = recursive(cmpInput, ch, numLetter);
+	end = clock();
+	timespent = ((double) (end - start)) / CLOCKS_PER_SEC;	
+	printf("ANS : %c\ntime spend : %lf\n", cmpInput,timespent);
+	free(ch);
+
+	return 0;
 }
